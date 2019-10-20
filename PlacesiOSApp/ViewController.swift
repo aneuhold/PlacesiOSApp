fff14bc5d20cfd697ef9e5ebe0e418ffbc2fa484
Anton G Neuhold Jr <agneuhold@gmail.com>
2019-10-20 16:59:54 -0700

    Initial Commit

---
+++ PlacesiOSApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlacesiOSApp
+//
+//  Created by Anton G Neuhold Jr on 10/20/19.
+//  Copyright © 2019 Anton G Neuhold Jr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
